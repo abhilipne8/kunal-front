d4aa91bc7765d24c6dfb947777757fc3ba326331
Abhishek Lipne <abhilipne2017@gmail.com>
2025-03-21 09:59:32 +0530

    some vite changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://13.60.11.86:5000", // Replace with your backend URL
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  }
+});
